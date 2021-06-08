8da0965840725e98d71b3df372ab537f235ce430
Fritz Larco <flarco@live.com>
2021-06-08 22:13:01 +0000

    fix paths for build

--- scripts/prep.gomod.sh
+++ scripts/prep.gomod.sh
@@ -2,5 +2,7 @@ set -e  # exit on error
 
 go mod edit -dropreplace='github.com/flarco/g' go.mod
 go mod edit -dropreplace='github.com/flarco/dbio' go.mod
+go mod edit -dropreplace='github.com/slingdata-io/sling' go.mod
 go get github.com/flarco/g@HEAD
 go get github.com/flarco/dbio@HEAD
+go get github.com/slingdata-io/sling@HEAD
