7078fe2f9f8400b18386f56f2a568d9370d89ac7
Fritz Larco <flarco@live.com>
2023-05-29 14:53:05 -0300

    update prep.gomod.sh

--- scripts/prep.gomod.sh
+++ scripts/prep.gomod.sh
@@ -1,8 +1,11 @@
 set -e  # exit on error
 
+
 go mod edit -dropreplace='github.com/flarco/g' go.mod
 go mod edit -dropreplace='github.com/flarco/dbio' go.mod
+go mod edit -dropreplace='github.com/dbrest-io/dbrest' go.mod
 go mod edit -dropreplace='github.com/slingdata-io/sling-cli' go.mod
 go get github.com/flarco/g@HEAD
 go get github.com/flarco/dbio@HEAD
 go get github.com/slingdata-io/sling-cli@HEAD
+go get github.com/dbrest-io/dbrest@HEAD
