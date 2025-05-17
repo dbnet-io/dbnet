85b0072dd3b39fbc47aaf76426fce5c77fa6747e
Fritz Larco <flarco@live.com>
2025-05-17 09:23:34 -0300

    ✨ feat(ui): add ui components
    - added `components.json` for UI configuration
    - added `package.json` and `package-lock.json` for UI dependencies
    - added `Button` component with various variants and sizes
    - updated `App.tsx` to use the `Button` component
    - added utility functions in `utils.ts` for className merging
    - updated `tsconfig.json` and `tsconfig.app.json` for path aliases and configurations
    - updated `vite.config.ts` to include tailwindcss plugin and resolve aliases
    - added `index.css` for tailwindcss styles and dark mode customization

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,9 +19,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <Button onClick={() => setCount((count) => count + 1)} className='cursor-pointer'>
           count is {count}
-        </button>
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
